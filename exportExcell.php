9309b1ef53786d515014f02d4963ba2d0da082b2
l1k1rc <45100859+l1k1rc@users.noreply.github.com>
2019-01-31 20:26:23 +0100

    export EXCELL

---
+++ exportExcell.php
@@ -0,0 +1,31 @@
+<?PHP
+  // Original PHP code by Chirp Internet: www.chirp.com.au
+  // Please acknowledge use of this code by including this header.
+  require('connectDatabase.php');
+  function cleanData(&$str)
+  {
+    $str = preg_replace("/\t/", "\\t", $str);
+    $str = preg_replace("/\r?\n/", "\\n", $str);
+    if(strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
+  }
+
+  // filename for download
+  $filename = "website_data_" . date('Ymd') . ".xls";
+
+  header("Content-Disposition: attachment; filename=\"$filename\"");
+  header("Content-Type: application/vnd.ms-excel");
+
+  $flag = false;
+  $dbconn=connectionDB();
+  $result = pg_query("SELECT student_idu,nameu,surnameu,genderu,emailu,phoneu,adru,typeu,validationu FROM users ORDER BY student_idu") or die('Query failed!');
+  while(false !== ($row = pg_fetch_assoc($result))) {
+    if(!$flag) {
+      // display field/column names as first row
+      echo implode("\t", array_keys($row)) . "\r\n";
+      $flag = true;
+    }
+    array_walk($row, __NAMESPACE__ . '\cleanData');
+    echo implode("\t", array_values($row)) . "\r\n";
+  }
+  exit;
+?>
