dd23fcc54a364b60cf72e16fa5abb07699dbf346
GuillaumeDCRX <g.ducroux@outlook.fr>
2019-02-07 16:46:17 +0100

    Ajout de la page tuteur + modif du bord

---
+++ tuteur.php
@@ -0,0 +1,40 @@
+<?php
+  /*Barrasset Raphaël, Castelain Julien, Ducroux Guillaume, Saint-Amand Matthieu  L3i 2019
+  raphael.barrasset@gmail.com, julom78@gmail.com, g.ducroux@outlook.fr, throwaraccoon@gmail.com*/
+    require('functions/main.func.php');
+    require('functions/tuteur.func.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="style.less" media="screen">
+  <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+  </html>
+    <?php
+      
+      $menu = displayMenu();
+      echo $menu;
+      
+
+    ?>
+    <!-- Barre de recherch dynamique gérée par script.js et style avec style.less -->
+    <div class="search__container">
+      <form id="auto-suggest" action="profil.php?" method="get">
+        <input type="text" class="search__input" name="search" value="Rechercher..." onfocus="if(this.value=='Rechercher...')this.value=''" autocomplete="off"/>
+        <ul class="suggestions">
+      <!-- remplit par le script -->
+        </ul>
+      </form>
+    </div>
+
+    <?php
+          $tuteurbord = dispBordtuteur();
+          echo $tuteurbord;
+    ?>    
+  </html>
+</body>
