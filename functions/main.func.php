1502fcf5493598d16f4954b2edcdf96d7c450a9c
rbarrass <raphael.barrasset@gmail.com>
2019-02-10 15:51:13 +0100

    Séparation du menu etdes icons dans la fct displayMenu

--- functions/main.func.php
+++ functions/main.func.php
@@ -1,18 +1,19 @@
 <?php
 function autorizedChar($strchain, $index){
-		//name/surname
-		if($index==0)	return preg_match('/^[a-zA-Z-ëéèàù]{1,}$/', $strchain);
-		//Phone number/age
-		elseif ($index==1)	return preg_match('/^[0-9]{1,}$/', $strchain);
+    //name/surname
+    if($index==0) return preg_match('/^[a-zA-Z-ëéèàù]{1,}$/', $strchain);
+    //Phone number/age
+    elseif ($index==1)  return preg_match('/^[0-9]{1,}$/', $strchain);
 }
+
 
 function displayMenu(){
 
-	$dbconn = connectionDB();
+  $dbconn = connectionDB();
 
 
 
-	$result = '<nav class="sidenav">
+  $result = '<nav class="sidenav">
         <ul class="main-buttons">
           <li>
             <i class="fa fa-circle fa-2x"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAB2wAAAdsBV+WHHwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAFWSURBVEiJ7ZaxSgNBFEXvSAISUEFFAmrnDwjBLxFLxUKw0SIi+CsKqa0Ff0BbwUqwSqOFihaaoGFROBbuYlyS3TebWbTwdMPO3jOz894y0i/hirwE1CTV4+G9c+4t3JIGCxeAI6DLN13gEJgvS7oCPDKcB6ARWjobB+dxB0xbMseM7j1Jc4Z5dUlNY2Y+wLVhtwlXlkxTVQM9SePGdfacc7VQYozSr1DncnOtZxycPy9+98iMQopvPcQ3IcWnHuITj7nZAEvAq6GHO8BiMHEsXwOiDGkErAaV9slbGeKWT5ZvO30UfDayOOuPVOhSkQlQATbj4hnGE7ABVEIIJ4FdoG2o6IQ2sANMFN3hAfDiIUzzDOybvwAwA5yPIExzRt6tBKgClwGlCRdAtd+VruptScs+x2KkIWkrS7xegnRg9o/eAzqS/KvRRsc5N5UM0hV3XJL0H30C47KaOwL3mHsAAAAASUVORK5CYII=" alt=""></i>
@@ -29,14 +30,14 @@ function displayMenu(){
 
             <ul class="hidden">
 
- 			<li onclick="location.href=\'index.php?psd=Alternants\';">Alternants</li>';
+      <li onclick="location.href=\'index.php?psd=Alternants\';">Alternants</li>';
             $query = 'SELECT branchcl FROM classrooms';
-			$res = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
-			while ($line = pg_fetch_array($res, null, PGSQL_ASSOC)) {
-    			$result.='
-					  <li onclick="location.href=\'index.php?psd='.$line["branchcl"].'\';">'.$line["branchcl"].'</li>';
+      $res = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
+      while ($line = pg_fetch_array($res, null, PGSQL_ASSOC)) {
+          $result.='
+            <li onclick="location.href=\'index.php?psd='.$line["branchcl"].'\';">'.$line["branchcl"].'</li>';
  
-			}
+      }
           $result.='</ul></li>
           <li>
             <i class="fa fa-circle fa-2x"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAADFwAAAxcBwpsE1QAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAFKSURBVGiB7ZlBTsNADEW/EULsqYpULsQ9ECekqypwHyrWSKGLz6ZI6TTROIwTWZbfqkoyHv+x/0yUCgwgyZbxIiKtOdy0BvDClRCSTyTfSX6zgnUytfnOOXUkd5pgnSLghRDt84ZxDmXeV71JsgdwB+BRRI7aldQ8N4XWIyS3AD4B9CJyfxFjKqk5BlxLyHCucsztnMEtCSw9j0pIDWtB/yHM9ttUkVZvWBKmIinEGya7lgbNztbiuaxIyd+KT63q0jtcmIqEEdLUWmMGHl4btlOaXYnZK0qa3YgwQszOkVrrpNmVrPaulWZXEkaI+cneQpod+fHBH2GEpNm9kWb3RhghaXZvpNm9kUK8EUaIyuwe/rWtEaYiY0J+AIDkw8q5VCG5Of/sy3tjrfUB4BnAl6NjoqQrL4xV5AXAHsBp8XTmcwLwBuC1vPELePhshbpEcpgAAAAASUVORK5CYII=" alt="" style="width: 33px;"></i>
@@ -76,8 +77,18 @@ function displayMenu(){
             </ul>
           </li>
           </ul>
-          </nav>
-          <div class="icons">
+          </nav>';
+          
+
+    closeDB($dbconn);
+
+    return $result;
+
+
+}
+
+function displayIcons(){
+  $result = '<div class="icons">
             <div class="iconsAlert">
               <img onclick="document.getElementById(\'id01\').style.display=\'block\'" src="https://img.icons8.com/ios-glyphs/30/000000/alarm.png" alt="" id="iconsAlertImg">
             </div>
@@ -126,9 +137,7 @@ function displayMenu(){
             }
           </script> ';
 
-
-
-    return $result;
+  return $result;
 
 
 }
