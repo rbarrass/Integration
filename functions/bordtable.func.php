7326fbe1d44cec4a160a92eae2ab2d4e7de563ca
rbarrass <raphael.barrasset@gmail.com>
2019-03-24 16:28:19 +0100

    Status

--- functions/bordtable.func.php
+++ functions/bordtable.func.php
@@ -12,7 +12,6 @@
 		            	 <th>Nom</th>
 		            	 <th>Prénom</th>
 		            	 <th>Email</th>
-		            	 <th>Statut</th>
 		            	 <th>Entreprise</th>
 		            	 <th>Téléphone</th>
 		            	 <th>Adresse</th>
@@ -24,7 +23,7 @@
 			$branch = $_GET['psd'];
 				if ($branch=="Alternants") {
 					$i=0;
-					$query2 = "SELECT student_idu, nameu, surnameu, emailu, validationu, nametut, namei, phoneu,adru,nationality,job,typejob,profilimgu FROM tutors INNER JOIN users ON tutors.idtut=users.idtut INNER JOIN institutions ON users.idi=institutions.idi WHERE typeu='student' AND validationu='allowed' ORDER BY nameu";
+					$query2 = "SELECT student_idu, nameu, surnameu, emailu, nametut, namei, phoneu,adru,nationality,job,typejob,profilimgu FROM tutors INNER JOIN users ON tutors.idtut=users.idtut INNER JOIN institutions ON users.idi=institutions.idi WHERE typeu='student' AND validationu='allowed' ORDER BY nameu";
 					$res2 = pg_query($query2) or die('Echec de la requête : ' .pg_last_error());
 					while ($line2 = pg_fetch_array($res2, null, PGSQL_ASSOC)) {
 							$i++;
@@ -46,7 +45,6 @@
 									             <td>'.$line2["nameu"].'</td>
 									             <td>'.$line2["surnameu"].'</td>
 									             <td>'.$line2["emailu"].'</td>
-									             <td>'.$line2["validationu"].'</td>
 									             <td>'.$line2["namei"].'</td>
 									             <td>'.$line2["phoneu"].'</td>
 									             <td>'.$line2["adru"].'</td>
@@ -64,7 +62,7 @@
 					$res1 = pg_query($query1) or die('Échec de la requête : ' . pg_last_error());
 					$line1 = pg_fetch_array($res1, null, PGSQL_ASSOC);
 					$id = $line1["idcl"];
-					$query = "SELECT student_idu, nameu, surnameu, emailu, validationu, nametut, namei, profilimgu FROM tutors INNER JOIN users ON tutors.idtut=users.idtut INNER JOIN institutions ON users.idi=institutions.idi WHERE idcl='$id' AND typeu='student' AND validationu='allowed' ORDER BY nameu";
+					$query = "SELECT student_idu, nameu, surnameu, emailu, nametut, namei, profilimgu FROM tutors INNER JOIN users ON tutors.idtut=users.idtut INNER JOIN institutions ON users.idi=institutions.idi WHERE idcl='$id' AND typeu='student' AND validationu='allowed' ORDER BY nameu";
 					$res = pg_query($query) or die('Échec de la requête : ' . pg_last_error());
 					while ($line = pg_fetch_array($res, null, PGSQL_ASSOC)) {
 						// binary data unescaped
@@ -80,7 +78,6 @@
 									             <td>'.$line["nameu"].'</td>
 									             <td>'.$line["surnameu"].'</td>
 									             <td>'.$line["emailu"].'</td>
-									             <td>'.$line["validationu"].'</td>
 									             <td>'.$line["namei"].'</td>
 								      		</tr>';
 			 
@@ -101,17 +98,23 @@
 			}
 			if (!empty($_GET['promo'])) {
 					$promo = $_GET['promo'];
-					$query3 = "SELECT student_idu, nameu, surnameu, emailu, validationu, nametut, namei FROM tutors INNER JOIN users ON tutors.idtut=users.idtut INNER JOIN institutions ON users.idi=institutions.idi WHERE promotionu='$promo' AND typeu='student' AND validationu='allowed' ORDER BY nameu";
+					$query3 = "SELECT student_idu, nameu, surnameu, emailu, nametut, namei, profilimgu FROM tutors INNER JOIN users ON tutors.idtut=users.idtut INNER JOIN institutions ON users.idi=institutions.idi WHERE promotionu='$promo' AND typeu='student' AND validationu='allowed' ORDER BY nameu";
 					$res3 = pg_query($query3) or die('Echec de la requête : ' .pg_last_error());
 					while ($line3 = pg_fetch_array($res3, null, PGSQL_ASSOC)) {
+						// binary data unescaped
+			            $img = pg_unescape_bytea($line3["profilimgu"]);
+
+			            // create the piture's name
+			            $fin = $line2["student_idu"].".png";
+			            file_put_contents($fin, $img);
+
 							$result.='
 									      <tr>
-									             <td><img class="ppStudent" src="http://lorempixel.com/100/100/people/1" alt="" /></td>
+									             <td><img class="ppStudent" src="'.$fin.'" alt="" /></td>
 									             <td>'.$line3["student_idu"].'</td>
 									             <td>'.$line3["nameu"].'</td>
 									             <td>'.$line3["surnameu"].'</td>
 									             <td>'.$line3["emailu"].'</td>
-									             <td>'.$line3["validationu"].'</td>
 									             <td>'.$line3["namei"].'</td>
 								      	</tr>';
 					}
